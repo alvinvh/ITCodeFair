5aabb759ef28b0effbdde8eedad598f1280375e6
alamin-sarker <INferno@38>
2023-10-14 01:22:23 +0930

    seeting up the project (not working yet)

---
+++ DataAccessLayer/Context/DBEntitiesContext.cs
@@ -0,0 +1,23 @@
+﻿using DataAccessLayer.Context.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Context
+{   
+
+    public class DBEntitiesContext : DbContext
+    {
+        public DBEntitiesContext(DbContextOptions<DBEntitiesContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Doctor> Doctors { get; set; }
+       
+
+    }
+
+}
---
+++ DataAccessLayer/Context/Models/Doctor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Context.Models
+{
+    public class Doctor
+    {
+        [Key]
+        public int DoctorId { get; set; } // int
+        public string DoctorName { get; set; } // nvarchar(400)
+        public string Image_url { get; set; }
+        public string Specialist { get; set; } // nvarchar(400)
+        public string Qualification { get; set; } // nvarchar(400)
+        public string Certification { get; set; } // nvarchar(400)
+        public string Education { get; set; } // nvarchar(400)
+        public string Description { get; set; } // nvarchar(400)
+        public string Availability { get; set; } // nvarchar(400)
+        
+    }
+}
